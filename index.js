611feeed7e69b6f09ec0bd209c09e38868aa70db
Kaitlyn Manlove <kaitlyn.manlove@me.com>
2017-11-12 20:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return "I can't hear you!"
-  } if (string === uppercase) {
+  } else if (string === uppercase) {
     return "YES INDEED!"
-  } else {
-    
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
   } {
-  
+
 }
